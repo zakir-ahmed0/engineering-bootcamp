84274e1148598976a625d33afccc65723b1474fc
zakir-ahmed0 <zakiruk098@gmail.com>
2026-06-30 22:50:17 +0100

    Day 1 Python

---
+++ Python/Day 01/about_me.py
@@ -0,0 +1,10 @@
+============================================================================
+Name : Zakir Mohammad
+=====================
+University : University Of Hertfordshire
+=====================
+Degree: Msc Advanced Computer Science
+=====================
+Dream Companies : Nvidia , SpaceX , Palantir, Lockheed Martin , Arm , AMD 
+=====================
+============================================================================
