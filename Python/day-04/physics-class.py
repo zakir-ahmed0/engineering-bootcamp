e63e9b64f155d05b794888b134216325bde52f56
zakir-ahmed0 <zakiruk098@gmail.com>
2026-07-09 23:22:02 +0100

    completed functions module and mini project

---
+++ Python/day-04/physics-class.py
@@ -0,0 +1,46 @@
+#Codecademy's Getting ready for physics class functions mini project module
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+
+# Write your code below: 
+#function to convert f to c temp
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * 5/9
+  return c_temp
+
+f100_in_celsius = f_to_c(100)
+
+#function to convert c to f temp
+def c_to_f(c_temp):
+  f_temp = (c_temp) * (9/5) + 32
+  return f_temp
+
+c0_in_fahrenheit = c_to_f(0)
+
+#playing with get_force mass and acceleration formulas 
+def get_force(mass,acceleration):
+  return mass * acceleration
+
+train_force = get_force(train_mass,train_acceleration)
+print(train_force)
+print("The GE train supplies" + " " + str(train_force) + " " + "Newtons of force." )
+
+#energy formulas
+def get_energy(mass,c=3*10**8):
+  return mass * c ** 2
+
+bomb_energy = get_energy(bomb_mass)
+
+print("A 1kg bomb supplies" + str(bomb_energy) + "Joules")
+
+#work which is force multiplied by distance
+def get_work(mass,acceleration,distance):
+  force = get_force(mass,acceleration) * distance
+  return force
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+
+print("The GE train does " + str(train_work) + "Joules of work over" + str(train_distance) + "meters")
